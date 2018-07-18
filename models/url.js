f54d67b816f231c7433a8edb248e6cc0f27ad1f0
Maan Singh <maan.vs97@gmail.com>
2018-07-19 02:21:35 +0530

    models/url.js updated

--- models/url.js
+++ models/url.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var CounterSchema = Schema({
-    _id: {type: String, required: true},
+    _id: {type: String, default: 'url_count'/**required: true**/},
     seq: { type: Number, default: 0 }
 });
 
@@ -10,18 +10,18 @@ var counter = mongoose.model('counter', CounterSchema);
 
 // create a schema for our links
 var urlSchema = new Schema({
-  _id: {type: Number, index: true},
+  _number: {type: Number, index: true},
   long_url: String,
   created_at: Date
 });
 
 urlSchema.pre('save', function(next){
   var doc = this;
-  counter.findByIdAndUpdate({_id: 'url_count'}, {$inc: {seq: 1} }, function(error, counter) {
+  counter.findByIdAndUpdate({_id: 'url_count'}, {$inc: {seq: 1} }, {upsert: true , new: true}, function(error, counter) {
       if (error)
           return next(error);
       doc.created_at = new Date();
-      doc._id = counter.seq;
+      doc._number = counter.seq;
       next();
   });
 });
