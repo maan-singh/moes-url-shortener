403d90714c0a1fedef1c333fbd4a9b3ef09c1092
Maan Singh <maan.vs97@gmail.com>
2018-07-22 18:09:23 +0530

    pre-heroku

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ config.db = {};
 // the URL shortening host - shortened URLs will be this + base58 ID
 // i.e.: http://localhost:3000/3Ys
 //config.webhost = 'http://localhost:3000/';
-config.webhost = 'https://maan-singh.herokuapp.com/';
+//config.webhost = 'https://maan-singh.herokuapp.com/';
+config.webhost = 'moes.xyz';
 
 // your MongoDB host and database name
 config.db.host = 'localhost';
